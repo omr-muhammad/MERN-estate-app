a0445cbd36fb6a458891460a2ee98fc80d130d07
omar-muhammad <omarelsayedmuhammad@gmail.com>
2024-01-09 09:53:20 +0200

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, `Username mustn't be empty`],
+    unique: [true, `This username is already used please try another one`],
+  },
+  email: {
+    type: String,
+    required: [true, `email mustn't be empty`],
+    unique: [true, `This email is already used please try another one`],
+  },
+  password: {
+    type: String,
+    required: [true, `Password mustn't be empty`],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
