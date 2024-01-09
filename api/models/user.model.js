5a9d6f0250a804691926d13ffac4db1d8c97df92
omar-muhammad <omarelsayedmuhammad@gmail.com>
2024-01-09 09:54:40 +0200

    added the timestamps to user schema

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,21 +1,24 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, `Username mustn't be empty`],
-    unique: [true, `This username is already used please try another one`],
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, `Username mustn't be empty`],
+      unique: [true, `This username is already used please try another one`],
+    },
+    email: {
+      type: String,
+      required: [true, `email mustn't be empty`],
+      unique: [true, `This email is already used please try another one`],
+    },
+    password: {
+      type: String,
+      required: [true, `Password mustn't be empty`],
+    },
   },
-  email: {
-    type: String,
-    required: [true, `email mustn't be empty`],
-    unique: [true, `This email is already used please try another one`],
-  },
-  password: {
-    type: String,
-    required: [true, `Password mustn't be empty`],
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
