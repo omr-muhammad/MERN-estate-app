6087ef85f58a094e277ab88e0c35c2d50ed9f2d5
omar-muhammad <omarelsayedmuhammad@gmail.com>
2024-01-15 16:00:46 +0200

    completed the sign in functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Header from "./components/Header";
+import SignTemplate from "./pages/SignTemplate";
 
 export default function App() {
   return (
@@ -12,8 +11,11 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="sign-in" element={<SignIn />} />
-        <Route path="sign-up" element={<SignUp />} />
+        <Route
+          path="sign-in"
+          element={<SignTemplate key={"signin"} isSigninPage={true} />}
+        />
+        <Route path="sign-up" element={<SignTemplate key={"signup"} />} />
         <Route path="profile" element={<Profile />} />
         <Route path="about" element={<About />} />
       </Routes>
