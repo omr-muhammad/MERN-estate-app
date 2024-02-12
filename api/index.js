44585db84924ade8c9fa05cf24e86830b7ea779e
omar-muhammad <omarelsayedmuhammad@gmail.com>
2024-02-12 10:00:11 +0200

    prepared the app for deploying

--- api/index.js
+++ api/index.js
@@ -1,20 +1,23 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
 
-import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js";
-import listingRouter from "./routes/listing.route.js";
-import globalErrorHandler from "./controller/error.controller.js";
-import CreateError from "./utils/error.utils.js";
-import cookieParser from "cookie-parser";
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import globalErrorHandler from './controller/error.controller.js';
+import CreateError from './utils/error.utils.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.DB.replace("<PASSWORD>", process.env.DB_PASSWORD))
-  .then(() => console.log("Connect to DB Successfuly."))
+  .connect(process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD))
+  .then(() => console.log('Connect to DB Successfuly.'))
   .catch((err) => console.log(err));
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -28,11 +31,16 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
-app.all("*", (req, res, next) => {
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+app.all('*', (req, res, next) => {
   next(CreateError(`This ${req.originalUrl} Is Not On The Server`, 404));
 });
 
