d03b409b0270457ca7d360c78bad5f19aa976537
omar-muhammad <omarelsayedmuhammad@gmail.com>
2024-03-24 10:36:41 +0200

    fixed home link in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,40 +1,40 @@
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import SearchInput from "./SearchInput";
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import SearchInput from './SearchInput';
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
 
   return (
-    <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Dori</span>
-            <span className="text-slate-700">Estate</span>
+    <header className='shadow-md bg-slate-200'>
+      <div className='flex items-center justify-between max-w-6xl p-3 mx-auto'>
+        <Link to='/'>
+          <h1 className='flex flex-wrap text-sm font-bold sm:text-xl'>
+            <span className='text-slate-500'>Dori</span>
+            <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
         <SearchInput />
-        <ul className="flex gap-4">
-          <Link to="/home">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+        <ul className='flex gap-4'>
+          <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
               Home
             </li>
           </Link>
-          <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+          <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
               About
             </li>
           </Link>
-          <Link to="/profile">
+          <Link to='/profile'>
             {currentUser ? (
               <img
-                className="rounded-full h-7 w-7 object-cover"
+                className='object-cover rounded-full h-7 w-7'
                 src={currentUser.avatar}
-                alt="avatar"
+                alt='avatar'
               />
             ) : (
-              <li className=" text-slate-700 hover:underline">Sign in</li>
+              <li className=' text-slate-700 hover:underline'>Sign in</li>
             )}
           </Link>
         </ul>
